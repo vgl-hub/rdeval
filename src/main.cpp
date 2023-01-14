16cf9eb52d083551ab790d6fa9e113114f591ffb
gf777 <giulio.formenti@gmail.com>
2023-01-14 15:46:22 -0800

    use rmChrFromStr to read filter

--- src/main.cpp
+++ src/main.cpp
@@ -155,11 +155,7 @@ int main(int argc, char **argv) {
                 
                 userInput.filter = optarg;
                 
-                userInput.filter.erase(remove(userInput.filter.begin(), userInput.filter.end(), '\''));
-                userInput.filter.erase(remove(userInput.filter.begin(), userInput.filter.end(), '\\'));
-                
-                std::cout<<"here"<<std::endl;
-                exit(1);
+                rmChrFromStr(userInput.filter, "\'\\");
 
                 if (!(userInput.filter[0] == '>' || userInput.filter[0] == '<' || userInput.filter[0] == '=') || !isInt(userInput.filter.substr(1))) {
                     printf ("Could not parse filter: %s \n", userInput.filter.c_str());
