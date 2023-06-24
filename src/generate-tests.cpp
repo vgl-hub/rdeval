4b1213678b800862c28a282ee94419ea3df620ed
gf777 <giulio.formenti@gmail.com>
2023-06-24 18:29:44 -0400

    syntax amenable for win

--- src/generate-tests.cpp
+++ src/generate-tests.cpp
@@ -37,7 +37,7 @@ int main(int, char **argv) {
     std::string exePath = getExePath(argv[0]);
 
     const std::map<std::set<std::string>, std::vector<std::string>> ext_args = {
-        {{"fasta", "fasta.gz", "fastq", "fastq.gz"}, {"", "5", "-f '>10'", "-f '<10'", "-f '=10'"}}
+        {{"fasta", "fasta.gz", "fastq", "fastq.gz"}, {"", "5", "-f \">10\"", "-f \"<10\"", "-f \"=10\""}}
     //  {{set of test file extensions}, {list of command line args to run with}}
     };
 
