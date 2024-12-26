6f306feca1e71b3a76fc50c2904c91a2a7ac5c23
Giulio Formenti <giulio.formenti@gmail.com>
2024-12-25 22:07:13 -0500

    Update reads.cpp

--- src/reads.cpp
+++ src/reads.cpp
@@ -775,7 +775,7 @@ void InReads::writeToStream() {
             }
             case 2:  { // fastq[.gz]
                 
-                for (std::vector<std::pair<std::vector<InRead*>,uint32_t>>::iterator it = readBatchesCpy.begin(); it != readBatchesCpy.end()) {
+                for (std::vector<std::pair<std::vector<InRead*>,uint32_t>>::iterator it = readBatchesCpy.begin(); it != readBatchesCpy.end();) {
                     
                     auto &inReads = *it;
                     
