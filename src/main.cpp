6ed92793b2cc85d1e67401b8158283d8b8f5676b
Giulio Formenti <giulio.formenti@gmail.com>
2026-02-25 16:11:16 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 #include "stream-obj.h"
 #include "input.h"
 
-std::string version = "0.0.8";
+std::string version = "0.0.9";
 
 //global
 std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now(); // immediately start the clock when the program is run
